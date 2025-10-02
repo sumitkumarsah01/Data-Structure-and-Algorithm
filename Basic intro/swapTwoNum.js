ab20216a71c1465fd327f6bada4b48a8e85547d7
Sumit Kumar Sah <sumitsah9801@gmail.com>
2025-10-02 11:24:55 +0530

    Today Is The First Day For Learning Dsa Learn the Basic

---
+++ Basic intro/swapTwoNum.js
@@ -0,0 +1,20 @@
+// 1st way
+let a=10;
+let b=30;
+
+// //using third variable
+// let temp=a;
+// a=b;
+// b=temp;
+// console.log(a,b)
+
+
+//2nd way
+// a=a+b;
+// b=a-b;
+// a=a-b;
+// console.log(a,b);
+
+//3rd way
+[a,b]=[b,a]
+console.log(a,b);
---
+++ Basic intro/unaryOperator.js
@@ -0,0 +1,29 @@
+// let i=10;
+// i=i++ + ++i;
+// console.log(i);
+
+
+// let a=10 ,b=20 ,c;
+
+// c=a+b+a++ + b++ + ++a + ++b;
+// console.log("a="+a);
+// console.log("b="+b);
+// console.log("c="+c);
+
+
+// let b=true;
+// b++;
+// console.log(b)
+
+// let i=0;
+// i=i++ - --i + ++i - i++;
+// console.log(i)
+
+
+
+// let i=1,j=2,k=3;
+// let m=i-- - j-- - k--;
+// console.log(m)
+// console.log(i)
+
+
