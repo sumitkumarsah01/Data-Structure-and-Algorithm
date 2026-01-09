b3cac5aef0e417965a7982fe8ef3a60a0fb84411
Sumit Kumar Sah <sumitsah9801@gmail.com>
2026-01-09 22:39:40 +0530

    print Hello World

---
+++ Function/helloWorld.java
@@ -0,0 +1,11 @@
+public class helloWorld {
+    public static void helloWorld(){
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        
+    }
+    public static void main(String[] args) {
+        helloWorld();  
+    } 
+}
