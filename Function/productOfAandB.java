d579ea1c299780981d0a0a3c99fd7cb1b18c629c
Sumit Kumar Sah <sumitsah9801@gmail.com>
2026-01-09 22:40:28 +0530

    Product Of two Number

---
+++ Function/productOfAandB.java
@@ -0,0 +1,12 @@
+public class productOfAandB {
+    public static int product(int num1,int num2){
+        int product=num1*num2;
+        return product;
+    }
+    public static void main(String[] args) {
+        int result=product(2, 6);
+        System.out.println(result);
+        
+    }
+    
+}
