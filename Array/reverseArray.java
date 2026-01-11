c4c320f5d9cb3b4def4a09f829ae8035aa895af5
Sumit Kumar Sah <sumitsah9801@gmail.com>
2026-01-11 23:20:09 +0530

    Reverse Array

---
+++ Array/reverseArray.java
@@ -0,0 +1,21 @@
+public class reverseArray {
+    public static void reverse(int arr[]){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int temp=arr[end];
+            arr[end]=arr[start];
+            arr[start]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10,20,12,14,16,18,20};
+        reverse(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
